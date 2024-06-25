744fe0c27acc304ad761969ad0ea9f6b6603bae5
gfferreiira <arzhun7381@gmail.com>
2024-06-25 20:57:49 -0300

    teste

--- Migrations/20240625235338_FuncionarioCreate.Designer.cs
+++ Migrations/20240625235338_FuncionarioCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoInterDisciplinar.Data;
 
@@ -11,9 +12,11 @@ using ProjetoInterDisciplinar.Data;
 namespace ProjetoInterDisciplinar.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240625235338_FuncionarioCreate")]
+    partial class FuncionarioCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
